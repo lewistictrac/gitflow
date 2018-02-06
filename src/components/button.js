828aceb4ffde5a41ec204277dee75f129cb84535
TicTracLewis <lewis.bellwood@tictrac.com>
2018-02-06 15:56:44 +0000

    button-component: add button js file and basic code

---
+++ src/components/button.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Button extends Component {
+  render() {
+    return (
+      <div className="Button">
+        <input type="button" value="hello world" />
+      </div>
+    );
+  }
+}
+
+export default Button;
