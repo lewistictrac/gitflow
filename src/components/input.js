3167debc3904518922cfd0e2d1ccf08373ddb3e2
TicTracLewis <lewis.bellwood@tictrac.com>
2018-02-06 16:06:54 +0000

    input-component: add input js file

---
+++ src/components/input.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Input extends Component {
+  render() {
+    return (
+      <div >
+      	<input className="Input" type="text" placeholder="Hello World"/>
+      </div>
+    );
+  }
+}
+
+export default Input;
