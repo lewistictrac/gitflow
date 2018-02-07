d1b60eb19a51899b2ef9c3a5d3a682d7cfe81efb
TicTracLewis <lewis.bellwood@tictrac.com>
2018-02-07 11:13:12 +0000

    paragraph-graph: js - add cmponent

---
+++ src/components/paragraph.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+
+class Title extends Component {
+  render() {
+    return (
+      <div className="Pargraph" >
+      	<p >Hello World</p>
+      </div>
+    );
+  }
+}
+
+export default Title;
