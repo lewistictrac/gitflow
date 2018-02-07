d21153c041b31eca9d2abb4f6b5f184b8ae5d51f
TicTracLewis <lewis.bellwood@tictrac.com>
2018-02-07 11:15:46 +0000

    h3-component: js - file created

---
+++ src/components/h3-component.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+
+class H3 extends Component {
+  render() {
+    return (
+      <div className="h3" >
+      	<h3 >Hello World</h3>
+      </div>
+    );
+  }
+}
+
+export default H3;
